bea93623032177445ab5e8ee57676cd1172aa760
Anton Eriksson <antonake@icloud.com>
2024-04-17 15:22:59 +0000

    Add the access adn refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-mm-f68b541c99c7.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
